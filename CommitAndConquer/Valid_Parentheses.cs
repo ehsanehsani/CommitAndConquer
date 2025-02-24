e275d9ed1379d3dab75744b3f07d6500a3104fc1
ehsan <pc.ehsan@gmail.com>
2025-02-24 17:21:30 +0100

    add valid parentheses

---
+++ CommitAndConquer/Valid_Parentheses.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommitAndConquer
+{
+    public class Valid_Parentheses
+    {
+        public static bool IsValid(string s)
+        {
+            var stack = new Stack<char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '[' || s[i] == '{' || s[i] == '(')
+                {
+                    stack.Push(s[i]);
+                }
+                else if (s[i] == ']' ||  s[i] == '}' ||  s[i] == ')')
+                {
+                    if (stack.Count <= 0) return false;
+
+                    var item = stack.Pop();
+
+                    switch (item)
+                    {
+                        case '(' when s[i] != ')': return false;
+                        case '[' when s[i] != ']': return false;
+                        case '{' when s[i] != '}': return false;
+                    }
+                }
+            }
+
+            if (stack.Count == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ CommitAndConquerTests/Valid_ParenthesesTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommitAndConquer;
+using FluentAssertions;
+
+namespace CommitAndConquerTests
+{
+    public class Valid_ParenthesesTests
+    {
+        [Test]
+        public void Test()
+        {
+            var result = Valid_Parentheses.IsValid("([])");
+            result.Should().BeTrue();
+        }
+    }
+}
